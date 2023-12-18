d0d34fdb866dd2b08964a1787dea49cd7e55f475
Nomthizzle <pumzam9@gmail.com>
2023-12-18 05:27:30 -0800

    mtask

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
 import sys
-
-
 def safe_function(fct, *args):
     try:
         result = fct(*args)
@@ -10,4 +7,3 @@ def safe_function(fct, *args):
     except Exception as i:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
-
